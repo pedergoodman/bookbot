392546eccfdbf5e08559114d8b8039e4dfd453d6
Peder Goodman <peder.goodman@gmail.com>
2025-06-22 21:52:49 -0500

    add course functions

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from stats import get_book_text
 from stats import count_words
 from stats import count_characters
+from stats import sort_dictionary
+from stats import create_report
+import sys
+
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    # print(book_text)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_book = sys.argv[1]
+    book_text = get_book_text(path_to_book)
+
     num_words = count_words(book_text)
+    print(f"{num_words} words found in {path_to_book}")
 
-    print(f"{num_words} words found in the document")
-
-
-    count_characters(book_text)
-
+    # count_characters(book_text)
+    # sort_dictionary(count_characters(book_text))
+ 
+    create_report(book_text)
 
 main()
--- stats.py
+++ stats.py
@@ -18,4 +18,42 @@ def count_characters(book_text):
         else: 
             char_dict.update({char: 1})
 
-    print(char_dict)
+    # print(char_dict)
+    return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+
+# accepts dictionary, returns sorted dict list
+def sort_dictionary(dict):
+    new_list = []
+    for key, value in dict.items():
+        # print(f"{key} : {value}")
+        # pass on special characters
+        if key.isalpha():
+        # create a k:v pair
+            new_dict = {"char": key, "num": value}
+        # add it to the list
+            new_list.append(new_dict)
+
+    #sort list
+    new_list.sort(reverse=True, key=sort_on)
+    # print(new_list)
+    return new_list
+
+
+# 
+def create_report(book_text):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print("--------- Character Count -------")
+
+    sorted_chars = sort_dictionary(count_characters(book_text))
+    
+    for item in sorted_chars:
+        char = item['char']
+        num = item['num']
+        print(f"{char}: {num}")
